496666f5f47b0ec3f307635348a926958ea353f4
elisaalvz <32070700+elisaalvz@users.noreply.github.com>
2017-11-06 19:46:19 -0600

    nnn

---
+++ application/controllers/Bitacora/bitacora.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bitacora extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		/************ Librerias para llamar funciones predefenidas **********/
+		$this->load->helper(array('url','form','funciones_rapidas'));
+		$this->load->model('modulos_model');
+	}
+
+	public function index(){
+		$this->load->view('templates/header');
+		$this->load->view('bitacora/bitacora');
+		$this->load->view('templates/footer');
+	}
+
+
+	public function gestionar_modulos(){
+		if($this->input->post('band') == "save"){
+			$data = array(
+			'nombre' => ucfirst(strtolower($this->input->post('nombre'))), 
+			'icono' => $this->input->post('icono'),
+			'descripcion' => $this->input->post('descripcion'),
+			'url' => $this->input->post('url'),
+			'opciones' => $this->input->post('opciones'),
+			'orden' => $this->input->post('orden'),
+			'dependencia' => $this->input->post('dependencia'),
+			'id_sistema' => $this->input->post('id_sistema')
+			);
+			echo $this->modulos_model->insertar_modulo($data);
+
+		}else if($this->input->post('band') == "edit"){
+
+			$data = array(
+			'idmodulo' => $this->input->post('idmodulo'), 
+			'nombre' => ucfirst(strtolower($this->input->post('nombre'))), 
+			'icono' => $this->input->post('icono'),
+			'descripcion' => $this->input->post('descripcion'),
+			'url' => $this->input->post('url'),
+			'opciones' => $this->input->post('opciones'),
+			'orden' => $this->input->post('orden'),
+			'dependencia' => $this->input->post('dependencia'),
+			'id_sistema' => $this->input->post('id_sistema')
+			);
+			echo $this->modulos_model->editar_modulo($data);
+
+		}else if($this->input->post('band') == "delete"){
+
+			$data = array(
+			'idmodulo' => $this->input->post('idmodulo'),
+			'dependencia' => $this->input->post('dependencia'),
+			'id_sistema' => $this->input->post('id_sistema')
+			);
+			echo $this->modulos_model->eliminar_modulo($data);
+
+		}
+	}
+
+	public function ordenar_modulo(){
+		$data = array(
+			'idmodulo' => $this->input->post('idmodulo'), 
+			'orden' => $this->input->post('orden'),
+			'dependencia' => $this->input->post('dependencia')
+			);
+		echo $this->modulos_model->ordenar_modulo($data);
+	}
+}
+?>
