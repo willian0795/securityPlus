a871af3b756f846f716febc2e4d151dc798f5aeb
willian0795 <sonryjr@gmail.com>
2017-11-10 10:43:30 -0600

    bitacora funcion

---
+++ application/models/Bitacora_model.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bitacora_model extends CI_Model {
+	
+	function __construct(){
+		parent::__construct();
+	}
+
+	function bitacora($data){
+
+		$data['id_sistema']
+		$data['descripcion'] 
+		$data['id_accion'] insert into sep_bitacora values 
+        $user = $this->session->userdata('usuario');
+        $fecha = date('Y-m-d h:i:s');
+        $ip = $this->get_real_ip();
+        
+		
+		$id = $this->obtener_ultimo_id("sep_bitacora","id_bitacora");
+
+		if($this->db->insert('sep_bitacora', array('id_bitacora' => $id, 'id_sistema' => $data['id_sistema'], 'id_usuario' => $user, 'descripcion' => $data['descripcion'], 'fecha' => $fecha, 'ip' => $ip, 'id_accion' => $data['id_accion']))){
+			return "exito";
+		}else{
+			return "fracaso";
+		}
+	}
+
+	public function get_real_ip(){
+
+        if (isset($_SERVER["HTTP_CLIENT_IP"]))
+        {
+            return $_SERVER["HTTP_CLIENT_IP"];
+        }
+        elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+        {
+            return $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }
+        elseif (isset($_SERVER["HTTP_X_FORWARDED"]))
+        {
+            return $_SERVER["HTTP_X_FORWARDED"];
+        }
+        elseif (isset($_SERVER["HTTP_FORWARDED_FOR"]))
+        {
+            return $_SERVER["HTTP_FORWARDED_FOR"];
+        }
+        elseif (isset($_SERVER["HTTP_FORWARDED"]))
+        {
+            return $_SERVER["HTTP_FORWARDED"];
+        }
+        else
+        {
+            return $_SERVER["REMOTE_ADDR"];
+        }
+    }
+
+}
+?>
