7d8101950710fa2e516cb966ed49e9d645ccf61d
willian0795 <sonryjr@gmail.com>
2017-11-23 21:22:36 -0600

    Login probablemente finalizado

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -8,7 +8,34 @@ class Login_model extends CI_Model {
 	}
 
 	function verificar_usuario($data){
-		$query = $this->db->query("SELECT * FROM org_usuario WHERE usuario = '".$data['usuario']."' AND password = '".md5($data['password'])."'");
+		$query = $this->db->query("SELECT * FROM org_usuario WHERE usuario = '".$data['usuario']."'");
+		if($query->num_rows() > 0){
+			return "existe";
+		}else{
+			return "falta";
+		}
+	}
+
+	function verificar_estado($data){
+		$query = $this->db->query("SELECT * FROM org_usuario WHERE usuario = '".$data['usuario']."' AND estado = 1");
+		if($query->num_rows() > 0){
+			return "activo";
+		}else{
+			return "inactivo";
+		}
+	}
+
+	function verificar_usuario_password($data){
+		$query = $this->db->query("SELECT * FROM org_usuario WHERE usuario = '".$data['usuario']."' AND password = '".$data['password']."' AND estado = 1");
+		if($query->num_rows() > 0){
+			return "existe";
+		}else{
+			return "falta";
+		}
+	}
+
+	function get_data_user($data){
+		$query = $this->db->query("SELECT * FROM org_usuario WHERE usuario = '".$data['usuario']."' AND estado = 1");
 		return $query;
 	}
 
